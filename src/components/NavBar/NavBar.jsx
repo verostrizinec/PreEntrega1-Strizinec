2a9f6e5d36d2b2ca39c010b8a478bfd46b10d078
Vero Strizinec <verostrizinec@gmail.com>
2023-08-31 21:54:17 -0300

    Repositorio del proyecto de React

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+import './navbar.css'
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <img src="C:\Users\veros\Desktop\coder-house\proyectoReact\tritatoTienda\img"></img>
+
+            <div>Item List Container</div>
+        </div>
+    )
+}
+
+export default Navbar
