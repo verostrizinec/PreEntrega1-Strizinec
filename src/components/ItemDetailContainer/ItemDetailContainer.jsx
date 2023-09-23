d47a47a406387c0eda0219cff008ed76cc36e5e7
Vero Strizinec <verostrizinec@gmail.com>
2023-09-23 19:09:43 -0300

    modificando itemdetailcontainer

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -2,23 +2,6 @@ import React, { useEffect, useState } from 'react';
 import ItemDetail from '../ItemDetail/ItemDetail';
 
 const ItemDetailContainer = () => {
-    const [item, setItem] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
-
-    useEffect(() => {
-        // Simulamos una llamada asincrónica a un mock después de 2 segundos
-        setTimeout(() => {
-            const mockItem = {
-                id: '1',
-                nombre: 'Producto 1',
-                descripcion: 'Descripción del Producto 1',
-                imagen: 'url-de-la-imagen',
-                precio: 10.99,
-            };
-            setItem(mockItem);
-            setIsLoading(false);
-        }, 2000);
-    }, []);
 
     return (
         <div>
